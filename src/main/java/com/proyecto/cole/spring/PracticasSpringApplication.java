5f7a39ec81fdc2a5b57d1b6cd6a3d280a669ad85
DantonMontecinos <dmontecinos28@gmail.com>
2024-02-12 14:05:58 -0300

    Inicio proyecto

---
+++ src/main/java/com/proyecto/cole/spring/PracticasSpringApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.cole.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticasSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticasSpringApplication.class, args);
+	}
+
+}
