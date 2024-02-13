7cb45978ea5f525dfedada7f13c040007a1e87ad
DantonMontecinos <dmontecinos28@gmail.com>
2024-02-13 14:16:22 -0300

    Modificando tablas, creando metodo Get para los usuarios

--- src/main/java/com/proyecto/cole/spring/controllers/UsuarioConroller.java
+++ src/main/java/com/proyecto/cole/spring/controllers/UsuarioConroller.java
@@ -1,13 +1,23 @@
 package com.proyecto.cole.spring.controllers;
 
+import com.proyecto.cole.spring.models.Usuario;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UsuarioConroller {
-    @RequestMapping(value= "prueba")
-    public String prueba(){
-        return "prueba";
+    @RequestMapping(value= "usuario/{id}")
+    public Usuario getUsuario(@PathVariable Long id){
+
+        Usuario usuario  = new Usuario();
+        usuario.setNombre("Cole");
+        usuario.setApellido("Monecinos");
+        usuario.setEmail("cole@mail.com");
+        usuario.setTelefono("35100020");
+        usuario.setId(id);
+
+        return usuario;
 
     }
 }
---
+++ src/main/resources/static/js/usuarios.js
@@ -0,0 +1,21 @@
+// Call the dataTables jQuery plugin
+$(document).ready(function() {
+  cargarUsuarios();
+  $('#usuarios').DataTable();
+});
+
+async function cargarUsuarios(){
+
+
+      const request = await fetch('usuario/2342', {
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+
+      });
+      const usuarios = await request.json();
+      console.log(usuarios);
+
+}
